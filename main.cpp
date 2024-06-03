6aca4417aa269d379423941fa132b7971ea591e6
SandSnip3r <sandsnip3r@gmail.com>
2024-06-03 12:37:39 -0700

    Reduced required qt version

--- main.cpp
+++ main.cpp
@@ -16,6 +16,22 @@ int main(int argc, char *argv[]) {
       Qt::QueuedConnection);
   engine.load(url);
 
+#if QT_VERSION >= QT_VERSION_CHECK(6, 5, 0)
   auto *sorry = engine.singletonInstance<SorryBackend*>("Sorry","SorryBackend");
+#else
+  // Register the singleton type
+  qmlRegisterSingletonType<SorryBackend>("Sorry", 1, 0, "SorryBackend", [](QQmlEngine *engine, QJSEngine *scriptEngine) -> QObject* {
+      Q_UNUSED(engine)
+      Q_UNUSED(scriptEngine)
+      return new SorryBackend();
+  });
+
+  // Get the type ID
+  int sorryBackendTypeId = qMetaTypeId<SorryBackend*>();
+
+  // Obtain the singleton instance
+  auto *sorry = engine.singletonInstance<SorryBackend*>(sorryBackendTypeId);
+#endif
+
   return app.exec();
 }
