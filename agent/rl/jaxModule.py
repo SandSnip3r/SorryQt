8b7c9b488bdb84092cdb4c7e44fd5156551df54e
Victor Stone <SandSnip3r@gmail.com>
2024-10-30 08:39:57 -0700

    Cleanup

--- agent/rl/jaxModule.py
+++ agent/rl/jaxModule.py
@@ -6,7 +6,7 @@ import pathlib
 from flax import nnx
 from functools import partial
 
-class SorryDenseModel(nnx.Module):
+class PolicyNetwork(nnx.Module):
   def __init__(self, rngs, actionSpaceSize):
     self.linear1 = nnx.Linear(in_features=327, out_features=32, rngs=rngs)
     self.linear2 = nnx.Linear(in_features=32, out_features=actionSpaceSize, rngs=rngs)
@@ -14,7 +14,9 @@ class SorryDenseModel(nnx.Module):
   def __call__(self, x):
     x = self.linear1(x)
     x = jax.nn.relu(x)
-    # Directly return the logits for both sampling (jax.random.categorical takes logits) and calculating probabilities
+    # Directly return the logits for
+    #  1. Sampling (jax.random.categorical takes logits)
+    #  2. Calculating probabilities
     return self.linear2(x)
 
 # ================================================================================================
@@ -29,6 +31,7 @@ def compiledUpdate(model, gradient, reward, learningRate):
 
 def getProbabilityAndIndex(key, model, data, mask):
   logits = model(data)
+  # `mask` is a 1D array of size `actionSpaceSize`, where the index of the action to be masked is set to -inf, and all other indices are set to 0
   maskedLogits = logits + mask
   selectedIndex = jax.random.categorical(key, maskedLogits)
   probabilities = jax.nn.softmax(maskedLogits)
@@ -43,25 +46,37 @@ def getProbabilitiesAndIndex(model, data, mask):
   return probabilities, jax.numpy.argmax(probabilities)
 
 def loadModelFromCheckpoint(checkpointPath, actionSpaceSize):
-  abstractModel = nnx.eval_shape(lambda: SorryDenseModel(rngs=nnx.Rngs(0), actionSpaceSize=actionSpaceSize))
+  abstractModel = nnx.eval_shape(lambda: PolicyNetwork(rngs=nnx.Rngs(0), actionSpaceSize=actionSpaceSize))
   graphdef, abstractState = nnx.split(abstractModel)
   checkpointer = ocp.StandardCheckpointer()
   stateRestored = checkpointer.restore(checkpointPath, abstractState)
   return nnx.merge(graphdef, stateRestored)
+
+def createNewModel(actionSpaceSize, rngs):
+  return PolicyNetwork(rngs=rngs, actionSpaceSize=actionSpaceSize)
+
 # ================================================================================================
 # ================================================================================================
 # ================================================================================================
 
 class InferenceClass:
   def __init__(self, actionSpaceSize):
+    # Save the checkpoint path
     checkpointPath = pathlib.Path(os.path.join(os.getcwd(), 'checkpoints')) / 'reinforce_1p_any'
     print(f'Loading model from {checkpointPath}')
-    self.model = loadModelFromCheckpoint(checkpointPath, actionSpaceSize)
+
+    # Load the model from checkpoint
+    self.policyNetwork = loadModelFromCheckpoint(checkpointPath, actionSpaceSize)
+
+    # Compile the inference functions
     self.getProbabilityIndex = nnx.jit(getProbabilityAndIndex)
     self.getProbabilitiesAndIndex = nnx.jit(getProbabilitiesAndIndex)
   
   def getProbabilitiesAndSelectedIndex(self, data, mask):
-    modelClone = nnx.clone(self.model)
+    # We're usually calling this function from another thread from C++. Due to JAX's trace contexts, we need to clone the model
+    # TODO: Manage the model at the C++ level
+    modelClone = nnx.clone(self.policyNetwork)
+
     return self.getProbabilitiesAndIndex(modelClone, data, mask)
 
 # ================================================================================================
@@ -70,28 +85,36 @@ class InferenceClass:
 
 class TrainingUtilClass:
   def __init__(self, actionSpaceSize, checkpointName=None):
+    # Initialize RNG
     self.rngs = nnx.Rngs(0, sampleStream=1)
+
+    # Save the checkpoint path
     self.checkpointPath = pathlib.Path(os.path.join(os.getcwd(), 'checkpoints')) / 'latest'
+
+    # Create the model, either from checkpoint or from scratch
     if checkpointName is not None:
       print(f'Loading model from {self.checkpointPath}')
-      self.model = loadModelFromCheckpoint(self.checkpointPath, actionSpaceSize)
+      self.policyNetwork = loadModelFromCheckpoint(self.checkpointPath, actionSpaceSize)
     else:
-      self.model = SorryDenseModel(rngs=self.rngs, actionSpaceSize=actionSpaceSize)
+      self.policyNetwork = createNewModel(actionSpaceSize, self.rngs)
+
+    # Initialize the checkpointer
     self.checkpointer = ocp.StandardCheckpointer()
 
+    # Compile the inference function, to be used later
     self.getProbabilityIndexAndGradient = nnx.jit(nnx.value_and_grad(getProbabilityAndIndex, argnums=1, has_aux=True))
   
   def setSeed(self, seed):
     self.rngs = nnx.Rngs(0, sampleStream=seed)
 
   def getGradientAndIndex(self, data, mask):
-    ((logProbability, index), gradient) = self.getProbabilityIndexAndGradient(self.rngs.sampleStream(), self.model, data, mask)
+    ((logProbability, index), gradient) = self.getProbabilityIndexAndGradient(self.rngs.sampleStream(), self.policyNetwork, data, mask)
     return gradient, index
 
   def update(self, gradient, reward, learningRate):
-    compiledUpdate(self.model, gradient, reward, learningRate)
+    compiledUpdate(self.policyNetwork, gradient, reward, learningRate)
   
   def saveCheckpoint(self):
-    _, state = nnx.split(self.model)
+    _, state = nnx.split(self.policyNetwork)
     self.checkpointer.save(self.checkpointPath, state, force=True)
     print(f'Saved checkpoint at {self.checkpointPath}')
